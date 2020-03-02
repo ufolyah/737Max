91adbf2a9a603dce53217062c500dbcfb5f8f66a
xudfy@DESKTOP <xudfy@shanghaitech.edu.cn>
2020-03-02 18:56:41 -0500

    initial

---
+++ references/CS509.sample/src/airport/Airports.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package airport;
+
+import java.util.ArrayList;
+
+/**
+ * This class aggregates a number of Airport. The aggregate is implemented as an ArrayList.
+ * Airports can be added to the aggregate using the ArrayList interface. Objects can 
+ * be removed from the collection using the ArrayList interface.
+ * 
+ * @author blake
+ * @version 1.0
+ * @since 2016-02-24
+ *
+ */
+public class Airports extends ArrayList<Airport> {
+	private static final long serialVersionUID = 1L;
+	
+	
+}
---
+++ references/CS509.sample/src/driver/Driver.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package driver;
+
+import java.util.Collections;
+
+import airport.Airport;
+import airport.Airports;
+import dao.ServerInterface;
+
+/**
+ * @author blake
+ * @since 2016-02-24
+ * @version 1.0
+ *
+ */
+public class Driver {
+
+	/**
+	 * Entry point for CS509 sample code driver
+	 * 
+	 * This driver will retrieve the list of airports from the CS509 server and print the list 
+	 * to the console sorted by 3 character airport code
+	 * 
+	 * @param args is the arguments passed to java vm in format of "CS509.sample teamName" where teamName is a valid team
+	 */
+	public static void main(String[] args) {
+		if (args.length != 1) {
+			System.err.println("usage: CS509.sample teamName");
+			System.exit(-1);
+			return;
+		}
+		
+		String teamName = args[0];
+		// Try to get a list of airports
+		Airports airports = ServerInterface.INSTANCE.getAirports(teamName);
+		Collections.sort(airports);
+		for (Airport airport : airports) {
+			System.out.println(airport.toString());
+		}
+	}
+}
