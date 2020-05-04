ab09ad821a1c2dd987ce67015821451c15f06084
robertadwan <37348958+robertadwan@users.noreply.github.com>
2020-05-04 15:27:42 -0400

    Added HTTP GET and POST request tests

--- src/test/B737Max/Components/ServerAPIAdapterTest.java
+++ src/test/B737Max/Components/ServerAPIAdapterTest.java
@@ -4,7 +4,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,29 +19,59 @@ class ServerAPIAdapterTest {
     void setup() throws IOException
     {
         instance = ServerAPIAdapter.getInstance();
+        Airplanes.getInstance().setList(new Airplane[0]);
+        Airports.getInstance().setList(new Airport[0]);
     }
     
     @Test
     void httpGet() throws IOException{
+        // GET request for Airplanes
+        System.out.println("----- GET Airplanes -----");
         assertTrue(Airplanes.getInstance().getList().length == 0);
         instance.getAirplanes();
         assertTrue(Airplanes.getInstance().getList().length > 0);
         
+        // GET request for Airports
+        System.out.println("----- GET Airports -----");
         assertTrue(Airports.getInstance().getList().length == 0);
         instance.getAirports();
         assertTrue(Airports.getInstance().getList().length > 0);
         
+        // GET request for departing flights
+        System.out.println("----- GET Departing Flights -----");
         Flight[] departingFlights = instance.getDepartureFlights(Airports.getInstance().selectByCode("BOS"),
-                LocalDate.of(2019, 5, 10));
-        for(Flight f: departingFlights) System.out.println(f);
+                LocalDate.of(2020, 5, 10));
         assertTrue(departingFlights.length > 0);
     
+        // GET request for arriving flights
+        System.out.println("----- GET Arriving Flights -----");
         Flight[] arrivingFlights = instance.getArrivalFlights(Airports.getInstance().selectByCode("BOS"),
-                LocalDate.of(2019, 5, 10));
+                LocalDate.of(2020, 5, 10));
         assertTrue(arrivingFlights.length > 0);
+    
+        // Reset DB
+        System.out.println("----- Reset DB -----");
+        instance.reset();
     }
 
     @Test
-    void httpPost() {
+    void httpPost() throws IOException{
+        // LocK DB
+        System.out.println("----- Lock DB -----");
+        instance.lock();
+        
+        // Make a reservation
+        System.out.println("----- Resrvation HTTP POST -----");
+        instance.getAirports();
+        instance.getAirplanes();
+        Flight[] departingFlights = instance.getDepartureFlights(Airports.getInstance().selectByCode("BOS"),
+                LocalDate.of(2020, 5, 10));
+        Trips trips = new Trips();
+        trips.addTrip(new Trip(departingFlights[0], SeatClass.COACH));
+        instance.reserve(trips.getTrips());
+        
+        // Unlock DB
+        System.out.println("----- Unlock DB -----");
+        instance.unlock();
     }
 }
