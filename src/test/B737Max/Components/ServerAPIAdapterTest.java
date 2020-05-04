56c191781bff1cc6613306be7e1070e8ce24561c
robertadwan <37348958+robertadwan@users.noreply.github.com>
2020-05-04 14:23:08 -0400

    Started HTTP GET test caeses

--- src/test/B737Max/Components/ServerAPIAdapterTest.java
+++ src/test/B737Max/Components/ServerAPIAdapterTest.java
@@ -1,12 +1,41 @@
 package B737Max.Components;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ServerAPIAdapterTest {
+    static ServerAPIAdapter instance;
+    
+    @BeforeEach
+    void setup() throws IOException
+    {
+        instance = ServerAPIAdapter.getInstance();
+    }
+    
     @Test
-    void httpGet() {
+    void httpGet() throws IOException{
+        assertTrue(Airplanes.getInstance().getList().length == 0);
+        instance.getAirplanes();
+        assertTrue(Airplanes.getInstance().getList().length > 0);
+        
+        assertTrue(Airports.getInstance().getList().length == 0);
+        instance.getAirports();
+        assertTrue(Airports.getInstance().getList().length > 0);
+        
+        Flight[] departingFlights = instance.getDepartureFlights(Airports.getInstance().selectByCode("BOS"),
+                LocalDate.of(2019, 5, 10));
+        for(Flight f: departingFlights) System.out.println(f);
+        assertTrue(departingFlights.length > 0);
+    
+        Flight[] arrivingFlights = instance.getArrivalFlights(Airports.getInstance().selectByCode("BOS"),
+                LocalDate.of(2019, 5, 10));
+        assertTrue(arrivingFlights.length > 0);
     }
 
     @Test
